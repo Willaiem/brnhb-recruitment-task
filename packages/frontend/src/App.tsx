e5d2463b3c9389f8d6ff6daedce37a28608e8182
Willaiem <vvwill4u@gmail.com>
2022-09-04 18:13:10 +0200

    feat: created the skeleton of Form component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,29 @@
 import './App.css'
 
+const Form = () => {
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" required />
+
+      <label htmlFor="surname">Surname</label>
+      <input type="text" name="surname" id="surname" required />
+
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" required />
+
+      <label htmlFor="date">Event date</label>
+      <input type="date" name="date" id="date" required />
+      <button>Submit</button>
+    </form>
+  )
+}
+
+
 export const App = () => {
   return (
-    <div>test</div>
+    <main>
+      <Form />
+    </main>
   )
 }
