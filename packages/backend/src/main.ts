6176a118c29f98bfd96f7813d9b500e7b47fbc9c
Willaiem <vvwill4u@gmail.com>
2022-09-12 01:35:04 +0200

    refactor(backend): removed the useless validateError fn (now used as parseError)

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -11,14 +11,6 @@ await app.register(cors, {
   origin: ["http://127.0.0.1:5173", "http://localhost:5173"]
 })
 
-const validateError = (err: unknown) => {
-  try {
-    return ServerErrorSchema.validateSync(err)
-  } catch (_err) {
-    return { error: "Internal Server Error", message: 'Something went wrong :(' }
-  }
-}
-
 app.post('/', async (req) => {
   try {
     const { firstName, lastName, email, eventDate } = await FormFieldsSchema.validate(req.body)
