54a0835c6d42ca75e97b36c87e8a324a79b87f83
Willaiem <vvwill4u@gmail.com>
2022-09-11 23:44:43 +0200

    refactor: removed the statusCode from server error schema

--- packages/shared/schemas/ServerError.schema.ts
+++ packages/shared/schemas/ServerError.schema.ts
@@ -1,7 +1,6 @@
 import { InferType, number, object, string } from 'yup';
 
 export const ServerErrorSchema = object({
-  statusCode: number().required(),
   error: string().required(),
   message: string().required()
 })
