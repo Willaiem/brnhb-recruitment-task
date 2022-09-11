a8e58911b921c7eacf9ecb55936f3f247992f575
Willaiem <vvwill4u@gmail.com>
2022-09-12 01:28:16 +0200

    refactor: moved the server responses mock to the shared package

---
+++ packages/shared/mocks/serverResponses.ts
@@ -0,0 +1,2 @@
+export const SERVER_SUCCESS_RESPONSE = { message: 'User added successfully' }
+export const SERVER_ERROR_RESPONSE = { error: "Internal Server Error", message: 'Something went wrong :(' }
