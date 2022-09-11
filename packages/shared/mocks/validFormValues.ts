2a2032b953c97703bae8ad293ccc613d656dce40
Willaiem <vvwill4u@gmail.com>
2022-09-11 23:43:34 +0200

    refactor: moved the mocks to the shared package

---
+++ packages/shared/mocks/validFormValues.ts
@@ -0,0 +1,8 @@
+import { FormFields } from "@shared/schemas/FormFields.schema";
+
+export const MOCKED_VALID_FORM_VALUES: FormFields = {
+  firstName: 'John',
+  lastName: 'Smith',
+  email: 'john@gmail.com',
+  eventDate: '2017-05-01'
+}
