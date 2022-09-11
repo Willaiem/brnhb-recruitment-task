6be6f261d182cea6daec6529318edf907035da8b
Willaiem <vvwill4u@gmail.com>
2022-09-11 22:28:46 +0200

    chore(backend): setup the test deps & config

--- packages/backend/vite.config.ts
+++ packages/backend/vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: false
+  }
 })
